0f6a8e462aa319d9582732c752bf71eb3be22a6b
anspirer <anspirer@yandex.com>
2023-11-26 19:39:28 +0300

    Добавлены прототипы промо-слайдера и промо-акций
    Промо-слайдер, пока просто с одной картинкой и кнопки, без
    функционала. Промо-акции функционал работает, но без фото

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -23,6 +23,7 @@ var isSafari = window.safari !== undefined;
 //   document.querySelector('.btn_fav').style.padding = '21px 22px 20px 22px';
 
 // }
+
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if(window.scrollY==0){
@@ -64,6 +65,60 @@ window.onscroll = function () {
 
   prevScrollpos = currentScrollPos;
 };
-// });
-//
+
+$(document).ready(function(){ 
+	const slider = document.querySelector('.scroll-prom');
+	let isDown = false;
+	let startX;
+	let scrollLeft;
+	
+	slider.addEventListener('mousedown', (e) => {
+	  isDown = true;
+	  slider.classList.add('active');
+	  startX = e.pageX - slider.offsetLeft;
+	  scrollLeft = slider.scrollLeft;
+	});
+	slider.addEventListener('mouseleave', () => {
+	  isDown = false;
+	  slider.classList.remove('active');
+	});
+	slider.addEventListener('mouseup', () => {
+	  isDown = false;
+	  slider.classList.remove('active');
+	});
+	slider.addEventListener('mousemove', (e) => {
+	  if(!isDown) return;
+	  e.preventDefault();
+	  const x = e.pageX - slider.offsetLeft;
+	  const walk = (x - startX) * 1.1; //scroll-fast
+	  slider.scrollLeft = scrollLeft - walk;
+	//   console.log(walk);
+	});
+	function updateText() {
+		var linkCallB = document.getElementById("fmobc");
+		var linkMobT = document.getElementById("fmobt");
+		var linkMobF = document.getElementById("fmobf");
+
+
+		if (window.innerWidth < 768) {
+			linkCallB.innerHTML = '<li>Обр. звонок</li>';
+			linkMobT.innerHTML = '<li>Телефон</li>';
+			linkMobF.innerHTML = '<li>Связаться</li>';
+			
+			
+		} else {
+			linkCallB.innerHTML = "<li>Обратный звонок</li>";
+			linkMobT.innerHTML = '<li>+7(999)999-99-99</li>';
+			linkMobF.innerHTML = '<li>Связаться с нами</li>';
+
+
+		}
+	}
+	
+	// Call the updateText function initially to set the initial text
+	// updateText();
+	
+	// Add an event listener to update the text when the window is resized
+	window.addEventListener("resize", updateText);
+});
 
